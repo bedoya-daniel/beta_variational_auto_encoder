9edf0fa378475e1e0d740e3d0baea7b14b106ea6
Wetzel Victor <wetzel.victor@gmail.com>
2017-11-25 01:16:05 +0100

    toyDataset and parameter space

---
+++ toyDataset/generateParameterSpace.py
@@ -0,0 +1,14 @@
+""" generateParameterSpace Module
+Generate the parameters' space according to the rules configured
+by the user
+"""
+
+# Librairies
+import numpy as np
+
+class parameterSpace:
+    def __init__(args):
+        paramF0 = {'f0' : np.arange(100, 1000, 50)}
+
+    def get_parameter_space(self):
+        return 0
