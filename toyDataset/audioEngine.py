b28ce6fb7cc317734e0cd2bdd7861bbf8dbdfddc
WetzelVictor <wetzel.victor@gmail.com>
2017-11-28 18:46:46 +0100

    audioEngine+unit-test/toydataset

--- toyDataset/audioEngine.py
+++ toyDataset/audioEngine.py
@@ -5,10 +5,10 @@ import librosa as lib
 import numpy as np
 
 class audioEngine:
-    def __init__(Fs=16000):
-        self.Fs = Fs
+    def __init__(self,Fs_Hz=16000):
+        self.Fs = Fs_Hz
         
-    def render_sample(self, params):
+    def render_sound(self, params, sound_length):
         """ Render the sample from a dictionnary of parameters
 
         INPUT: 
@@ -17,3 +17,20 @@ class audioEngine:
         OUTPUT:
             - Numpy array of size (N x 1) containing the sample
         """
+
+        return np.random.rand((sound_length))
+
+
+    def spectrogram(self, data):
+        """ an n-array of sounds into spectrograms """
+        # M: number of sounds
+        # N: number of samples
+        (M,N) = np.shape(data)
+        Nfft = 2048
+        spectrograms = [lib.stft(data[1], n_fft=Nfft)*0 for i in xrange(M)]
+
+        # FOR LOOP: computing spectrogram
+        for i in xrange(M):
+            spectrograms[i] = lib.stft(data[i], n_fft=Nfft)
+
+        return spectrograms
