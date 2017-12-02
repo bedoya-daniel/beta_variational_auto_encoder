0b1de52b28f40861ca8e3d954a28774c76a42415
WetzelVictor <wetzel.victor@gmail.com>
2017-12-02 17:44:53 +0100

    WIP: spectros are out. But need to work on changeable size of images and spectrograms

--- toyDataset/audioEngine.py
+++ toyDataset/audioEngine.py
@@ -5,8 +5,9 @@ import librosa as lib
 import numpy as np
 
 class audioEngine:
-    def __init__(self,Fs_Hz=16000):
+    def __init__(self,Fs_Hz=16000, n_fft=1024):
         self.Fs = Fs_Hz
+        self.n_fft = n_fft
         
     def render_sound(self, params, sound_length):
         """ Render the sample from a dictionnary of parameters
@@ -21,16 +22,17 @@ class audioEngine:
         return np.random.rand((sound_length))
 
 
-    def spectrogram(self, data, n_fft=1024):
-        """ an n-array of sounds into spectrograms """
+    def spectrogram(self, data):
+        """ Returns the spectrograms of the array of sounds 'data' """
         # M: number of sounds
         # N: number of samples
         (M,N) = np.shape(data)
-
-        spectrograms = [lib.stft(data[1], n_fft=n_fft)*0 for i in xrange(M)]
+        
+        # Allocating the memory needed
+        spectrograms = [lib.stft(data[1], n_fft=self.n_fft) * 0 for i in xrange(M)]
 
         # FOR LOOP: computing spectrogram
         for i in xrange(M):
-            spectrograms[i] = np.abs(lib.stft(data[i], n_fft=n_fft))
+            spectrograms[i] = np.abs( lib.stft(data[i], n_fft=self.n_fft) )
 
         return spectrograms
