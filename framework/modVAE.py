b02c9553f579a1bf30d82108c326baa8ca5be20c
WetzelVictor <vivou@DESKTOP-KHBBTFK.localdomain>
2017-12-11 09:32:45 +0100

    Everything is documented: need to check on the stability of the scripts

--- framework/modVAE.py
+++ framework/modVAE.py
@@ -9,20 +9,28 @@ from framework.utils import to_var
 
 # VAE model
 class VAE(nn.Module):
-    """ Variational audo-encoder class """
+    """ Variational Audo-Encoder (VAE) class """
 
     def __init__(self, image_size=784, h_dim=400, z_dim=5):
+        """ Creates a VAE net object. 
+        
+        Arguments:
+            - image_size (int, def: 784): size of the flattened image (1D)
+            - h_dim (int, def:400): size of the hidden layer
+            - z_dim (int, def:5): number of latent dimensions
+
+        Returns:
+            - VAE object
+        """
         super(VAE, self).__init__()
 
         # ENCODER
-        # Linear, LeakyReLU, Linear
         self.encoder = nn.Sequential(
             nn.Linear(image_size, h_dim),
             nn.LeakyReLU(0.2),
             nn.Linear(h_dim, z_dim*2))  # 2 for mean and variance.
 
         # DECODER
-        # Linear, ReLU, linear, Sigmoid
         self.decoder = nn.Sequential(
             nn.Linear(z_dim, h_dim),
             nn.ReLU(),
@@ -30,14 +38,34 @@ class VAE(nn.Module):
             nn.Sigmoid())
 
     def reparametrize(self, mu, log_var):
-        """"z = mean + eps * sigma where eps is sampled from N(0, 1)."""
+        """" Does the reparametrize trick on the beta-VAE:
+            $z = mean + eps * sigma$ where 'eps' is sampled from N(0, 1).
+
+        Arguments: 
+            - mu: mean of the distribution
+            - log_var: log value of the variance
+
+        Returns:
+            - output: Reparametrized data (mu + eps * exp(log_var/2))
+        
+        """
         eps = to_var(torch.randn(mu.size(0), mu.size(1)))
         output = mu + eps * torch.exp(log_var/2)    # 2 for convert var to std
         return output
 
     def forward(self, data):
-        """ forward(x):
-            Do the forward pass in the VAE model """
+        """ Do the forward pass in the VAE model: encodes, reparameterize and
+        decodes the input data.
+        
+        Arguments:
+            - data (array[SAMPLE_INDEX][DATA_VECTOR]):
+
+        Returns:
+            - output (array like data): data encoded, reparametrized then decoded
+            - mu: mean 
+            - log_var: log value of the variance
+        
+        """
         encoded_vec = self.encoder(data)
         mu, log_var = torch.chunk(encoded_vec, 2, dim=1)  # mean and log variance.
         reparam = self.reparametrize(mu, log_var)
@@ -45,9 +73,15 @@ class VAE(nn.Module):
         
         return output, mu, log_var
 
-    def sample(self, hiddenVec):
-        """ sample(hiddenVec):
-            Decodes data from the z space """
-        return self.decoder(hiddenVec)
+    def sample(self, latent_vec):
+        """ Decodes the given latent vector.
+        
+        Arguments:
+            - latent_vec: matrix containing a number of encoded vectors
+
+        Returns:
+            - output: decoded vector
+        """
+        return self.decoder(latent_vec)
 
 
--- toyDataset/dataset.py
+++ toyDataset/dataset.py
@@ -1,6 +1,12 @@
 #-*-encoding:UTF-8-*-
 """ Toy Dataset class:
-    Create a dataset depending on arguments """
+    Create a toyDataset object. 
+    Calls the modules
+    toydataset.generateParameterSpace and toydataset.audioEngine. The first
+    makes a cardinal product of the specficied parameters. The second contains
+    the method to synthesize the samples.
+    
+    """
 
 # Librairies
 import numpy as np
@@ -12,29 +18,27 @@ import librosa as lib
 import generateParameterSpace as gps
 import audioEngine as aud
 
-# Classe du toyDataset
 class toyDataset(Dataset):
     def __init__(self, Fe_Hz=8000, length_sample=64000, batch_size=100, n_fft=1024):
         """ ToyDataset object. When initialized, synthesized the dataset with
         the specifications defined in the parameter_space objet 
 
-        INPUT:
+        Arguments:
             -(opt) Fs: samplerate (def: 16kHz)
             -(opt) length_sample: number of sample per sound example (def:64000)
             -(opt) batch_size: OBSOLETE size of the dataset (def: 100)
             -(opt) n_fft: order of the n_fft analysis (def: 1024)
         
-        OUTPUT:
+        Returns:
             - toyDataset object
         """
-        # initializing variables
+        # Initializing scalar variables
         self.Fs = Fe_Hz
         self.batch_size = batch_size
         self.length_sample = length_sample
         self.n_fft = n_fft
 
         # init data structures
-        self.batch_parameters = {}
         self.sound_data = [];
         self.spectrograms = [];
 
@@ -48,10 +52,11 @@ class toyDataset(Dataset):
         self.render_dataset()
         
     def render_dataset(self):
-        """ Render the whole parameter space from the parameter space field 
+        """ Render the whole parameter space from the parameter space instance.
         
-        OUTPUT:
-            all the sounds of the parameter space
+        Returns:
+            - self.sound_data: array containing all the sampled
+            - self.spectrograms: array of spectrograms
         """
 
         # Allocating memory
@@ -65,39 +70,15 @@ class toyDataset(Dataset):
         self.spectrograms = self.audio_engine.spectrogram(self.sound_data)
         
 
-    def get_minibatch(self, batchSize=100, render=True):
-        """ 
-        DEPRECATED
-        
-        Outputs a dataset for the bVAE. If render = True, recalculate a new 
-        minibatch. If False, just return the old one (self.toyDataset)
-
-        INPUT:
-            - batchSize: number of list of parameter to be taken
-        """
-        if render:
-            self.batch_parameters = [{} for i in xrange(batchSize)]
-            self.batch_size = batchSize
-            
-            self.batch_parameters = self.get_rand_params(self.batch_size)
-
-            # Create sounds
-            self.sound_data = self.render_batch()
-
-            # Converts them into spectrograms
-            self.spectrograms = self.audio_engine.spectrogram(self.sound_data)
-
-        # returns the spectrograms
-        return self.spectrograms
-
     def __getitem__(self, index):
-        """ Returns a sample which is a dict with key "images" and "parameters"
+        """ Returns a tuple containing a spectrogram and the parameters that
+        were used to generate the sound.
 
-        INPUT:
-            - index: index corresponing to a sample in the dataset
+        Arguments:
+            - index: index corresponding to a sample in the dataset
 
-        OUTPUT:
-            - sample: dict {'image':spectrogram, 'parameters':[parameters]}
+        Returns:
+            - sample: dict {'image':[array: spectrogram], 'parameters':[parameters]}
 
         UNIT TEST:
             - Vérifier que la fonction retourne bien un dico avec un numpy array 
@@ -109,52 +90,12 @@ class toyDataset(Dataset):
         return image,param
 
     def __len__(self):
-        """ Returns the length of the label vector
-        OUTPUT:
-            - len: length of the parameter vector
+        """ Returns the length of the Dataset, e.g. the number of samples
+        
+        Returns
+            - len (int): number of sounds
+
+        UNIT TEST:
+            - Vérifier qu'il renvoit bien un int > 0
         """
         return len(self.sound_data)
-
-    def render_batch(self):
-        """ Render the given batch of samples from the input arguments. 
-        Default is the whole array of parameter toyDataset
-
-        INPUT:
-            - paramsPatch (array 1*N): N list of parameters 
-                                       for sample rendering
-        """
-        # INIT: allocate memory
-        sound_data = [[None in xrange(self.length_sample)] \
-                      for j in xrange(self.batch_size)]
-
-        # FOR LOOP: renders each sound
-        for i in xrange(self.batch_size):
-            sound_data[i] = self.audio_engine.\
-                    render_sound(self.batch_parameters[i],\
-                                self.length_sample)
-        
-        # Returns the rendered audio
-        return sound_data
-
-
-    def get_rand_params(self, batchSize):
-        """ Generate a set of parameters of size 'batchSize' 
-
-        INPUT:
-            - batchSize: Number of set of parameter to generate from the dict
-
-        OUTPUT:
-            - paramBatch: array of size [batchSize x numberOfParam] containing the 
-            sets of parameters
-
-        unit test: it sends back an array of the good size with no 'None' value
-        """
-        # INIT
-        batch_parameters = [{} for i in xrange(self.batch_size)]
-
-        # FOR LOOP: generates the batch of random parameters
-        for i in xrange(self.batch_size):
-            batch_parameters[i] = self.paramSpace.get_rand_parameters()
-
-        # Returns the data
-        return batch_parameters
